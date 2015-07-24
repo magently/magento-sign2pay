6063f63f03ae650e30de9975256d57512593f541
Paweł Tatarczuk <hello@magently.com>
2015-07-24 13:42:04 +0200

    Fix observer compatybility for magento 1.7.0.2 or less

--- app/code/community/Sign2pay/Payment/Model/Observer.php
+++ app/code/community/Sign2pay/Payment/Model/Observer.php
@@ -13,17 +13,37 @@ class Sign2pay_Payment_Model_Observer
         $transport = $observer->getTransport();
 
         if ($block instanceof Mage_Checkout_Block_Onepage_Shipping_Method_Available) {
-            if (Mage::getModel('sign2pay/sign2pay')->isApplicableToQuote(
-                    $block->getQuote(),
-                    Mage_Payment_Model_Method_Abstract::CHECK_USE_FOR_COUNTRY
-                    | Mage_Payment_Model_Method_Abstract::CHECK_USE_FOR_CURRENCY
-                    | Mage_Payment_Model_Method_Abstract::CHECK_ORDER_TOTAL_MIN_MAX
-                    | Mage_Payment_Model_Method_Abstract::CHECK_ZERO_TOTAL
-                ))
-            {
-                $html = '<script type="text/javascript">initializeRiskAssessment();</script>';
-                $transport->setHtml($transport->getHtml() . $html);
+            $payment = Mage::getModel('sign2pay/sign2pay');
+
+            if (method_exists($payment, 'isApplicableToQuote')) {
+                try {
+                    if ($payment->isApplicableToQuote(
+                            $block->getQuote(),
+                            Mage_Payment_Model_Method_Abstract::CHECK_USE_FOR_COUNTRY
+                            | Mage_Payment_Model_Method_Abstract::CHECK_USE_FOR_CURRENCY
+                            | Mage_Payment_Model_Method_Abstract::CHECK_ORDER_TOTAL_MIN_MAX
+                            | Mage_Payment_Model_Method_Abstract::CHECK_ZERO_TOTAL
+                        ))
+                    {
+                        $this->_addInitializeRiskAssessment($transport);
+                    }
+                } catch (Exception $e) {
+                    // Add anyway
+                    $this->_addInitializeRiskAssessment($transport);
+                }
+            } else {
+                // Add anyway
+                $this->_addInitializeRiskAssessment($transport);
             }
         }
+    }
+
+    /**
+     * Add riska assessment trigger to transport
+     */
+    protected function _addInitializeRiskAssessment($transport)
+    {
+        $html = '<script type="text/javascript">if (typeof initializeRiskAssessment === \'function\') initializeRiskAssessment();</script>';
+        $transport->setHtml($transport->getHtml() . $html);
     }
 }
